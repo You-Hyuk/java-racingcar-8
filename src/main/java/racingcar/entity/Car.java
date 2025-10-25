53ac0f1b5e700e18b60533e775c777c24063fbc7
You-Hyuk <cxz6411@naver.com>
2025-10-26 04:17:19 +0900

    feat(Car): Car 엔티티 구현

---
+++ src/main/java/racingcar/entity/Car.java
@@ -0,0 +1,13 @@
+package racingcar.entity;
+
+public class Car {
+
+    private String name;
+    private int winCount;
+
+    public Car(String name, int winCount) {
+        this.name = name;
+        this.winCount = winCount;
+    }
+
+}
