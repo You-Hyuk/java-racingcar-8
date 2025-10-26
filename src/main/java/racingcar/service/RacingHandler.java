f8aec4f4885edb95a08d060cc7240996adee7161
You-Hyuk <cxz6411@naver.com>
2025-10-27 07:02:20 +0900

    feat(RacingHandler): 자동차 경주 진행 메소드 구현

--- src/main/java/racingcar/service/RacingHandler.java
+++ src/main/java/racingcar/service/RacingHandler.java
@@ -1,6 +1,7 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 import racingcar.entity.Car;
 
@@ -9,6 +10,19 @@ public class RacingHandler {
     private final int MIN_BOUND = 0;
     private final int MAX_BOUND = 9;
     private final int MIN_PASS_VALUE = 4;
+    private final int DEFAULT_MOVE_COUNT = 0;
+
+    public void executeRace(List<String> nameList, int count) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : nameList) {
+            Car car = new Car(name, DEFAULT_MOVE_COUNT);
+            cars.add(car);
+        }
+
+        for (int round = 0; round < count; round++) {
+            race(cars);
+        }
+    }
 
     private void race(List<Car> cars) {
         for (Car car : cars) {
