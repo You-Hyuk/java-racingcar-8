402244c17d0803878f9e8e39326eb3573ed9bd5a
You-Hyuk <cxz6411@naver.com>
2025-10-27 12:56:05 +0900

    test(InputHandlerTest): 이름 입력 유효성 검증 및 파싱 테스트

---
+++ src/test/java/racingcar/service/InputHandlerTest.java
@@ -0,0 +1,50 @@
+package racingcar.service;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.exception.ErrorMessage;
+
+public class InputHandlerTest {
+
+    private final InputHandler inputHandler = new InputHandler();
+
+    @Test
+    @DisplayName("5자를 넘는 이름을 포함한 경우 예외가 발생한다.")
+    public void failIfNameLengthExceedFive() throws Exception {
+        //given
+        String names = "pobi,woni,jun,benson";
+
+        //when && then
+        Assertions.assertThatThrownBy(() -> inputHandler.parseName(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_NAME_LENGTH.getDescription());
+    }
+
+    @Test
+    @DisplayName("동일한 이름을 포함한 경우 예외가 발생한다.")
+    public void failIfDuplicateNameExist() throws Exception {
+        //given
+        String names = "pobi,woni,pobi,james";
+
+        //when && then
+        Assertions.assertThatThrownBy(() -> inputHandler.parseName(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.DUPLICATE_NAME.getDescription());
+    }
+
+    @Test
+    @DisplayName("쉼표(,)를 기준으로 이름을 파싱한다.")
+    public void parseNamesByCommaDelimiter() throws Exception {
+        //given
+        String names = "pobi,jun,woni";
+
+        //when
+        List<String> nameList = inputHandler.parseName(names);
+
+        //then
+        Assertions.assertThat(nameList)
+                .containsExactly("pobi", "jun", "woni");
+    }
+}
